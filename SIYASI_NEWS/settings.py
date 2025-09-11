f70779bf69d17108806e5c3652a43395ab2c794c
Subhan <businessofcoding@gmail.com>
2025-09-12 00:25:26 +0500

    adding App password

--- SIYASI_NEWS/settings.py
+++ SIYASI_NEWS/settings.py
@@ -129,3 +129,14 @@ STATIC_URL = '/static/'
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "businessofcoding@gmail.com"
+EMAIL_HOST_PASSWORD = 'yrlg xxth ilzx azvy'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
