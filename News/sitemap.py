851f8b9c208bc798db7d9b84dddef12226ffa046
Subhan <businessofcoding@gmail.com>
2025-10-26 22:39:39 +0500

    adding requirements

--- News/sitemap.py
+++ News/sitemap.py
@@ -1,13 +1,13 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
-from .models import Article  # use your article/news model
+from .models import NewsArticle  # use your article/news model
 
 class ArticleSitemap(Sitemap):
     changefreq = "daily"
     priority = 0.9
 
     def items(self):
-        return Article.objects.all()
+        return NewsArticle.objects.all()
 
     def lastmod(self, obj):
         return obj.updated_at  # or obj.date if you use date
--- SIYASI_NEWS/settings.py
+++ SIYASI_NEWS/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'News',
     'rest_framework',
-    'django.contrib.sites',
     'django.contrib.sitemaps',
 
 
