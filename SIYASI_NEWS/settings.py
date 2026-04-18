72968ab77ca14691d2b4634d346bb12d9b718754
Subhan <businessofcoding@gmail.com>
2026-04-18 16:05:52 +0500

    allowed host settings

--- SIYASI_NEWS/settings.py
+++ SIYASI_NEWS/settings.py
@@ -34,10 +34,10 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-dev-key')
 
 DEBUG = env_bool('DJANGO_DEBUG', True)
 
-ALLOWED_HOSTS = env_list('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1')
+ALLOWED_HOSTS = env_list('DJANGO_ALLOWED_HOSTS', 'siyasi-news.onrender.com,localhost,127.0.0.1:8000')
 CSRF_TRUSTED_ORIGINS = env_list(
     'DJANGO_CSRF_TRUSTED_ORIGINS',
-    'http://localhost:8000,http://127.0.0.1:8000',
+    'http://localhost:8000,http://127.0.0.1:8000,https://siyasi-news.onrender.com',
 )
 SERVE_MEDIA = env_bool('DJANGO_SERVE_MEDIA', DEBUG)
 
